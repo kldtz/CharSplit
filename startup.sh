5efba377fb07a2b205c08778556ee6d2f8029e61
James Shaw <jshaw@ebrevia.com>
2017-05-06 11:26:57 -0400

    enabled multithread gunicorn

--- startup.sh
+++ startup.sh
@@ -2,5 +2,6 @@
 
 export EB_MODELS='dir-scut-model'
 # gunicorn --workers 2 --timeout 600 app:app
-gunicorn --timeout 600 app:app
+# gunicorn --timeout 600 app:app
 
+gunicorn --timeout 1200 -k gevent --worker-connections 4 app:app
