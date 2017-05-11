716b7e5635ac55dc3da4b828ae7ab07fec7318fb
James Shaw <jshaw@ebrevia.com>
2017-05-11 12:14:33 -0400

    using copy-on-write facility in linux to support multiple workers

--- startup.sh
+++ startup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 export EB_MODELS='dir-scut-model'
-# gunicorn --workers 2 --timeout 600 app:app
-# gunicorn --timeout 600 app:app
-
-gunicorn --timeout 1200 -k gevent --worker-connections 4 app:app
+gunicorn --workers 4 --timeout 1200 --preload app:app
