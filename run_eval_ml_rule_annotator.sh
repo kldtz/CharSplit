b741fbedb0d6d6ca7fe479ae49e832c0892cc8ba
James Shaw <jshaw@ebrevia.com>
2017-08-21 14:08:59 -0400

    added a way to test rule-based annotation

---
+++ run_eval_ml_rule_annotator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+WORK_DIR=dir-work
+MODEL_DIR=dir-model
+SCUT_MODEL_DIR=dir-scut-model
+CUSTOM_MODEL_DIR=dir-custom-model
+
+
+main.py --cmd eval_ml_rule_annotator --provision $1 --docs $SCUT_MODEL_DIR/$1_test_doclist.txt --work_dir $WORK_DIR --model_dir $SCUT_MODEL_DIR --custom_model_dir $CUSTOM_MODEL_DIR
+
+
