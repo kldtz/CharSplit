977d394a672cd845d5e6a830ba55b339abb13785
emilytag <emily.tagtow@gmail.com>
2018-02-08 11:00:49 -0500

    comments and cleanup

--- kirke/sampleutils/addrgen.py
+++ kirke/sampleutils/addrgen.py
@@ -12,21 +12,29 @@ class AddrContextGenerator:
     def find_constituencies(self, text, constituencies):
         s = ''
         text = text.replace ("\n", " ")
+
+        #replaces words with consistuency information
         for word in text.split():
             word = re.sub(r'[,\.]+$|\-', "", word)
             if word.isdigit() or word in constituencies:
                 s += '1'
             else:
                 s += '0'
+
+        #matches based on sequence of constituencies
         matches = re.finditer(r'(1+0?0?(1+0?0?){1,3}1+)', s)
         all_spans = [match.span(1) for match in matches]
         ads = []
+
+        #checks matches to eliminate things that aren't addresses
         for ad_start, ad_end in all_spans:
             list_address = text.split()[ad_start:ad_end]
             ad_st = " ".join(list_address) 
             address_prob = addresses.classify(ad_st)
             if address_prob >= 0.5 and len(text.split()[ad_start:ad_end]) > 3:
                 ad_st = re.sub('[\(\.\)]', '', ad_st)
+
+                #finds address in text so we can recover the exact indices
                 for found in regex.finditer('(?e)(?:'+ad_st+'){e<=3}', text):
                     pred_start, pred_end = found.span()
                     ads_list = [pred_start, pred_end, text[pred_start:pred_end]]
@@ -41,8 +49,6 @@ class AddrContextGenerator:
         label_list = []   # type: List[bool]
         group_id_list = []  # type: List[int]
 
-
-        # each sample is the date regex +
         for group_id, antdoc in enumerate(antdoc_list):  # these are ebantdoc3
             
             #creates list of ants for a specific provision
@@ -62,10 +68,12 @@ class AddrContextGenerator:
 
             if group_id % 10 == 0:
                 logging.info("AddrContextGenerator.documents_to_samples(), group_id = {}".format(group_id))
-            
+           
+            #loads address keywords 
             all_keywords = addresses.all_constituencies()
             split_text = nl_text.split()
-            #print([x[2] for x in self.find_constituencies(nl_text, all_keywords)])
+
+            #finds all addresses in the text and adds window around each as a candidate
             for addr in self.find_constituencies(nl_text, all_keywords):
                 new_start, new_end, addr_st = addr
                 is_label = ebsentutils.check_start_end_overlap(new_start,
@@ -74,7 +82,7 @@ class AddrContextGenerator:
                 prev_n_words, prev_spans = strutils.get_lc_prev_n_words(nl_text, new_start, self.num_prev_words)
                 post_n_words, post_spans = strutils.get_lc_post_n_words(nl_text, new_end, self.num_post_words)
                 new_bow = '{} {} {}'.format(' '.join(prev_n_words), addr_st, ' '.join(post_n_words))
-                #print("\t>>>>>", new_bow)
+                #update span based on window size
                 if prev_spans:
                     new_start = prev_spans[0][0]
                 if post_spans:
@@ -93,80 +101,5 @@ class AddrContextGenerator:
                     label_list.append(True)
                 else:
                     label_list.append(False)
-            '''
-            lines = nl_text.split('\n')
-            offset = 0
-            notempty_line_seq = 0
-            prev_start = 0
-            #creates dict for candidate information
-            for i in range(0, len(lines)):
-                line = lines[i]
-                line_len = len(line)
-                start = offset
-                end = offset + line_len
-                new_bow = line.strip()
-                x = None
-                if not line:  # skip
-                    offset += len(line) + 1
-                    continue
-                is_label = ebsentutils.check_start_end_overlap(start,
-                                                               end,
-                                                               label_ant_list)
-                found_adresses = self.find_constituencies(line, all_keywords)
-                if not found_adresses:
-                    new_start = start
-                    new_end = end
-                    prev_n_words, prev_spans = strutils.get_lc_prev_n_words(nl_text, new_start, self.num_prev_words)
-                    post_n_words, post_spans = strutils.get_lc_post_n_words(nl_text, new_end, self.num_post_words)
-                    a_sample = {'sample_type': 'addr',
-                                'start': new_start,
-                                'end': new_end,
-                                'line_seq': notempty_line_seq,
-                                'text': new_bow,
-                                'prev_n_words': ' ' .join(prev_n_words),
-                                'post_n_words': ' '.join(post_n_words),
-                                'has_addr': False}
-                    samples.append(a_sample)
-                    group_id_list.append(group_id)
-                    if is_label:
-                        a_sample['label_human'] = label
-                        label_list.append(True)
-                    else:
-                        label_list.append(False)
-                else:
-                    for x in found_adresses: 
-                        ad_start = x[0]
-                        ad_end = x[1]
-                        new_start = ad_start + start
-                        new_end = ad_end + start
-                        prev_n_words, prev_spans = strutils.get_lc_prev_n_words(nl_text, new_start, self.num_prev_words)
-                        post_n_words, post_spans = strutils.get_lc_post_n_words(nl_text, new_end, self.num_post_words)
-                        new_bow = '{} {} {}'.format(' '.join(prev_n_words), x[2], ' '.join(post_n_words))
-                        #new_bow = '{} {}'.format(' '.join(prev_n_words), ' '.join(post_n_words))
-                        if prev_spans:
-                            new_start = prev_spans[0][0]
-                        else:
-                            new_start = ad_start
-                        if post_spans:
-                            new_end = post_spans[-1][-1]
-                        else:
-                            new_end = ad_end
-                        a_sample = {'sample_type': 'addr',
-                                    'start': new_start,
-                                    'end': new_end,
-                                    'line_seq': notempty_line_seq,
-                                    'text': new_bow,
-                                    'prev_n_words': ' '.join(prev_n_words),
-                                    'post_n_words': ' '.join(post_n_words),
-                                    'has_addr': True}
-                        samples.append(a_sample)
-                        group_id_list.append(group_id)
-                        if is_label:
-                            a_sample['label_human'] = label
-                            label_list.append(True)
-                        else:
-                            label_list.append(False)
-                notempty_line_seq += 1
-                offset += len(line) + 1  # for eoln
-            '''
+        
         return samples, label_list, group_id_list
