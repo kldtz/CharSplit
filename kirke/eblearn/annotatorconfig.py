e9d997b75d575db47d3e44b92454f44eeb06d7ad
emilytag <emily.tagtow@gmail.com>
2018-03-09 11:20:25 -0500

    l_tenant_notice removed from config

--- kirke/eblearn/annotatorconfig.py
+++ kirke/eblearn/annotatorconfig.py
@@ -58,26 +58,6 @@ ML_ANNOTATOR_CONFIG_LIST = [
                         'gridsearch_parameters': {'clf__alpha': 10.0 ** -np.arange(4, 6)},
                         'threshold': 0.25,
                         'kfold': 2}),
-
-    ('l_tenant_notice', '1.0', {'doclist_to_antdoc_list': ebantdoc3.doclist_to_ebantdoc_list,
-                                'docs_to_samples': addrgen.AddrContextGenerator(10, 2, 'ADDRESS'),
-                                'post_process_list': 'l_tenant_notice',
-                                'sample_transformers': [addrannotator.SampleAddAddrLineProb()],
-                                'version': "1.0",
-                                'pipeline': Pipeline([
-                                    ('union', FeatureUnion(
-                                        transformer_list=[
-                                            # pylint: disable=line-too-long
-                                            ('surround_transformer', transformerutils.SimpleTextTransformer()),
-                                            ('is_addr_line_transformer', transformerutils.AddrLineTransformer())
-                                        ])),
-                                    ('clf', SGDClassifier(loss='log', penalty='l2', n_iter=50,
-                                                          shuffle=True, random_state=42,
-                                                          class_weight={True: 3, False: 1}))]),
-                                'gridsearch_parameters': {'clf__alpha': 10.0 ** -np.arange(4, 6)},
-                                'threshold': 0.25,
-                                'kfold': 2}),
-
     ('CURRENCY', '1.0', {'doclist_to_antdoc_list': ebantdoc3.doclist_to_ebantdoc_list,
                          'docs_to_samples': regexgen.RegexContextGenerator(20,
                                                                            5,
@@ -139,7 +119,26 @@ ML_ANNOTATOR_CONFIG_LIST = [
                         'threshold': 0.25,
                         'kfold': 2})
 ]
-
+'''
+('l_tenant_notice', '1.0', {'doclist_to_antdoc_list': ebantdoc3.doclist_to_ebantdoc_list,
+                                'docs_to_samples': addrgen.AddrContextGenerator(10, 2, 'ADDRESS'),
+                                'post_process_list': 'l_tenant_notice',
+                                'sample_transformers': [addrannotator.SampleAddAddrLineProb()],
+                                'version': "1.0",
+                                'pipeline': Pipeline([
+                                    ('union', FeatureUnion(
+                                        transformer_list=[
+                                            # pylint: disable=line-too-long
+                                            ('surround_transformer', transformerutils.SimpleTextTransformer()),
+                                            ('is_addr_line_transformer', transformerutils.AddrLineTransformer())
+                                        ])),
+                                    ('clf', SGDClassifier(loss='log', penalty='l2', n_iter=50,
+                                                          shuffle=True, random_state=42,
+                                                          class_weight={True: 3, False: 1}))]),
+                                'gridsearch_parameters': {'clf__alpha': 10.0 ** -np.arange(4, 6)},
+                                'threshold': 0.25,
+                                'kfold': 2}),
+'''
 RULE_ANNOTATOR_CONFIG_LIST = [
     ('effectivedate', '1.0', {'doclist_to_antdoc_list': ebantdoc3.doclist_to_ebantdoc_list,
                               'docs_to_samples': dategen.DateSpanGenerator(20, 20, 'DATE'),
