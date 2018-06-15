b7400febaa7364337c3218b1f466624af2a2793b
emilytag <emily.tagtow@gmail.com>
2018-06-15 12:51:23 -0400

    regexgen refactor, alphanum test cases added

--- kirke/sampleutils/regexgen.py
+++ kirke/sampleutils/regexgen.py
@@ -26,49 +26,29 @@ class RegexContextGenerator:
         self.length_min = length_min
         self.group_num = group_num
 
-    # pylint: disable=too-many-locals
-    def documents_to_candidates(self,
-                                antdoc_list: List[ebantdoc4.EbAnnotatedDoc4],
-                                label: str = None) -> List[Tuple[ebantdoc4.EbAnnotatedDoc4,
-                                                               List[Dict],
-                                                               List[bool],
-                                                               List[int]]]:
+    def merge_candidates(self, cands: List[Dict], nl_text: str) -> Dict:
+        # doesn't need to be merged
+        if len(cands) == 1:
+            return cands[0]
 
-        if 'length_min' not in self.__dict__:
-            self.length_min = 0
-        if 'join' not in self.__dict__:
-            self.join = False
-        if 'group_num' not in self.__dict__:
-            self.group_num = 1
-        # pylint: disable=line-too-long
-        result = []  # type: List[Tuple[ebantdoc4.EbAnnotatedDoc4, List[Dict], List[bool], List[int]]]
-        for group_id, antdoc in enumerate(antdoc_list):  # these are ebantdoc4
-            candidates = []  # type: List[Dict]
-            label_list = []   # type: List[bool]
-            group_id_list = []  # type: List[int]
+        # else concat the list of candidates to a single dictionary
+        else:
+            new_cand = {'candidate_type': self.candidate_type,
+                        'bow_start': cands[0]['bow_start'],
+                        'bow_end': cands[-1]['bow_end'],
+                        'start': cands[0]['start'],
+                        'end': cands[-1]['end'],
+                        'prev_n_words': cands[0]['prev_n_words'],
+                        'post_n_words': cands[-1]['post_n_words']}
+            new_cand['text'] = nl_text[new_cand['bow_start']:new_cand['bow_end']]
+            new_cand['chars'] = nl_text[new_cand['start']:new_cand['end']]
+            return new_cand
 
-            #creates list of ants for a specific provision
-            ant_list = antdoc.prov_annotation_list
-            label_ant_list = []
-            for ant in ant_list:
-                if ant.label == label:
-                    label_ant_list.append(ant)
-
-            #gets text based on document type
-            if antdoc.doc_format in set([ebantdoc4.EbDocFormat.html,
-                                         ebantdoc4.EbDocFormat.html_nodocstruct,
-                                         ebantdoc4.EbDocFormat.other]):
-                nl_text = antdoc.text
-            else:
-                nl_text = antdoc.get_nl_text()
-
-            if group_id % 10 == 0:
-                logger.debug('RegexContextGenerator.documents_to_candidates(), group_id = %d',
-                             group_id)
-
-            #finds all matches in the text and adds window around each as a candidate
-            matches = self.center_regex.finditer(nl_text)
-            for match in matches:
+    def get_candidates_from_text(self, nl_text: str, group_id: int, label_ant_list: List[str], label_list: List[bool]):
+        candidates = [] # type: List[Dict]
+        group_id_list = [] # type: List[int]
+        matches = self.center_regex.finditer(nl_text)
+        for match in matches:
                 match_start, match_end = match.span(self.group_num)
                 match_str = match.group(self.group_num)
                 is_label = ebsentutils.check_start_end_overlap(match_start,
@@ -121,158 +101,110 @@ class RegexContextGenerator:
                     label_list.append(True)
                 else:
                     label_list.append(False)
-
-            # joins adjacent candidates that are only separated by whitespace
-            if self.join:
-                candidates_to_merge = []
-                new_candidates = []
-                merge_labels = []
-                merge_groups = []
-                i = 0
-                while i < len(candidates):
-                    skip = True
-                    candidates_to_merge.append(candidates[i])
-                    # looks ahead until it fails the requirement
-                    while skip and i+1 < len(candidates):
-                        diff = candidates[i+1]['start'] - candidates_to_merge[-1]['end']
-                        diff_str = nl_text[candidates_to_merge[-1]['end']:candidates[i+1]['start']]
-                        if (diff_str.isspace() or not diff_str) and diff < 3:
-                            candidates_to_merge.append(candidates[i+1])
-                            i += 1
-                        else:
-                            skip = False
-
-                    # merges candidates that pass the requirement
-                    merged_cands = self.merge_candidates(candidates_to_merge, nl_text)
-                    new_candidates.append(merged_cands)
-                    candidates_to_merge = []
-                    merge_labels.append(label_list[i])
-                    merge_groups.append(group_id_list[i])
-                    i += 1
-
-                candidates = new_candidates
-                label_list = merge_labels
-                group_id_list = merge_groups
-
-            # remove any candidate that is >= min_length
-            filtered_candidates = []  # type: List[Dict]
-            filtered_label_list = []  # type: List[bool]
-            filtered_group_id_list = []  # type: List[int]          
-            for candidate, label, group_id in zip(candidates,
-                                                  label_list,
-                                                  group_id_list):
-                if len(candidate['chars']) >= self.length_min:
-                    filtered_candidates.append(candidate)
-                    filtered_label_list.append(label)
-                    filtered_group_id_list.append(group_id)
-
-            result.append((antdoc, filtered_candidates,
-                           filtered_label_list, filtered_group_id_list))            
-
-        return result
-
-    def merge_candidates(self, cands: List[Dict], nl_text: str) -> Dict:
-        # doesn't need to be merged
-        if len(cands) == 1:
-            return cands[0]
-
-        # else concat the list of candidates to a single dictionary
-        else:
-            new_cand = {'candidate_type': self.candidate_type,
-                        'bow_start': cands[0]['bow_start'],
-                        'bow_end': cands[-1]['bow_end'],
-                        'start': cands[0]['start'],
-                        'end': cands[-1]['end'],
-                        'prev_n_words': cands[0]['prev_n_words'],
-                        'post_n_words': cands[-1]['post_n_words']}
-            new_cand['text'] = nl_text[new_cand['bow_start']:new_cand['bow_end']]
-            new_cand['chars'] = nl_text[new_cand['start']:new_cand['end']]
-            return new_cand
-
-def extract_doc_candidates(regex_pat: Pattern,
-                           group_num: int,
-                           atext: str,
-                           candidate_type: str,
-                           num_prev_words: int,
-                           num_post_words: int,
-                           min_length: int,
-                           is_join: bool) \
-    -> List[Dict]:
-
-    candidates = []  # type: List[Dict]
-    matches = regex_pat.finditer(atext)
-    for match in matches:
-        match_start, match_end = match.span(group_num)
-        match_str = match.group(group_num)
-        prev_n_words, prev_spans = strutils.get_prev_n_clx_tokens(atext,
-                                                                  match_start,
-                                                                  num_prev_words)
-        post_n_words, post_spans = strutils.get_post_n_clx_tokens(atext,
-                                                                  match_end,
-                                                                  num_post_words)
-        new_bow = '{} {} {}'.format(' '.join(prev_n_words),
-                                    match_str,
-                                    ' '.join(post_n_words))
-
-        #update span based on window size
-        new_start = match_start
-        new_end = match_end
-        if prev_spans:
-            new_start = prev_spans[0][0]
-        if post_spans:
-            new_end = post_spans[-1][-1]
-
-        # clean up the string if special character is at the end.  Currently
-        # none of the matat_str will have nose characters except for ";" or ":"
-        if match_str.endswith(',') or match_str.endswith(';') or \
-           match_str.endswith(':') or match_str.endswith('.'):
-            match_str = match_str[:-1]
-            match_end -= 1
-        if match_str.endswith(')') and not '(' in match_str:
-            match_str = match_str[:-1]
-            match_end -= 1
-        if match_str.startswith('(') and not ')' in match_str:
-            match_str = match_str[1:]
-            match_start += 1
-
-        a_candidate = {'candidate_type': candidate_type,
-                       'bow_start': new_start,
-                       'bow_end': new_end,
-                       'text': new_bow,
-                       'start': match_start,
-                       'end': match_end,
-                       'prev_n_words': ' '.join(prev_n_words),
-                       'post_n_words': ' '.join(post_n_words),
-                       'chars': match_str}
-        candidates.append(a_candidate)
-
-        if is_join:
-            merge_candidates = []
+        # joins adjacent candidates that are only separated by whitespace
+        if self.join:
+            candidates_to_merge = []
+            new_candidates = []
+            merge_labels = []
+            merge_groups = []
             i = 0
             while i < len(candidates):
                 skip = True
-                new_candidate = copy.deepcopy(candidates[i])
+                candidates_to_merge.append(candidates[i])
+                # looks ahead until it fails the requirement
                 while skip and i+1 < len(candidates):
-                    diff = candidates[i+1]['start'] - new_candidate['end']
-                    diff_str = atext[new_candidate['end']:candidates[i+1]['start']]
+                    diff = candidates[i+1]['start'] - candidates_to_merge[-1]['end']
+                    diff_str = nl_text[candidates_to_merge[-1]['end']:candidates[i+1]['start']]
                     if (diff_str.isspace() or not diff_str) and diff < 3:
-                        new_candidate['end'] = candidates[i+1]['end']
-                        new_candidate['chars'] = atext[new_candidate['start']:new_candidate['end']]
+                        candidates_to_merge.append(candidates[i+1])
                         i += 1
-                        if i == len(candidates) - 1:
-                            skip = False
-                            merge_candidates.append(new_candidate)
-                            i += 1
                     else:
-                        merge_candidates.append(new_candidate)
-                        i += 1
                         skip = False
-                if i == len(candidates) - 1:
-                    skip = False
-                    merge_candidates.append(candidates[i])
-                    i += 1
-            candidates = merge_candidates
-        
 
-    return [candidate for candidate in candidates
-            if len(candidate['chars']) >= min_length]
+                    # merges candidates that pass the requirement
+                merged_cands = self.merge_candidates(candidates_to_merge, nl_text)
+                new_candidates.append(merged_cands)
+                candidates_to_merge = []
+                merge_labels.append(label_list[i])
+                merge_groups.append(group_id_list[i])
+                i += 1
+
+            candidates = new_candidates
+            label_list = merge_labels
+            group_id_list = merge_groups
+        # remove any candidate that is >= min_length
+        filtered_candidates = []  # type: List[Dict]
+        filtered_label_list = []  # type: List[bool]
+        filtered_group_id_list = []  # type: List[int]
+        for candidate, label, group_id in zip(candidates,
+                                              label_list,
+                                              group_id_list):
+            if len(candidate['chars']) >= self.length_min:
+                filtered_candidates.append(candidate)
+                filtered_label_list.append(label)
+                filtered_group_id_list.append(group_id)
+        return filtered_candidates, filtered_group_id_list, filtered_label_list
+
+    # pylint: disable=too-many-locals
+    def documents_to_candidates(self,
+                                antdoc_list: List[ebantdoc4.EbAnnotatedDoc4],
+                                label: str = None) -> List[Tuple[ebantdoc4.EbAnnotatedDoc4,
+                                                               List[Dict],
+                                                               List[bool],
+                                                               List[int]]]:
+
+        if 'length_min' not in self.__dict__:
+            self.length_min = 0
+        if 'join' not in self.__dict__:
+            self.join = False
+        if 'group_num' not in self.__dict__:
+            self.group_num = 1
+        # pylint: disable=line-too-long
+        result = []  # type: List[Tuple[ebantdoc4.EbAnnotatedDoc4, List[Dict], List[bool], List[int]]]
+        for group_id, antdoc in enumerate(antdoc_list):  # these are ebantdoc4
+            label_list = []   # type: List[bool]
+
+            #creates list of ants for a specific provision
+            ant_list = antdoc.prov_annotation_list
+            label_ant_list = []
+            for ant in ant_list:
+                if ant.label == label:
+                    label_ant_list.append(ant)
+
+            #gets text based on document type
+            if antdoc.doc_format in set([ebantdoc4.EbDocFormat.html,
+                                         ebantdoc4.EbDocFormat.html_nodocstruct,
+                                         ebantdoc4.EbDocFormat.other]):
+                nl_text = antdoc.text
+            else:
+                nl_text = antdoc.get_nl_text()
+
+            if group_id % 10 == 0:
+                logger.debug('RegexContextGenerator.documents_to_candidates(), group_id = %d',
+                             group_id)
+
+            candidates, group_id_list, label_list = self.get_candidates_from_text(nl_text, group_id, label_ant_list, label_list)
+            result.append((antdoc, candidates, label_list, group_id_list))
+        return result
+
+    def extract_doc_candidates(regex_pat: Pattern,
+                               group_num: int,
+                               atext: str,
+                               candidate_type: str,
+                               num_prev_words: int,
+                               num_post_words: int,
+                               min_length: int,
+                               is_join: bool) -> List[Dict]:
+
+        candidates, group_id_list, label_list = self.get_candidates_from_text(nl_text, group_id, label_list)
+        return candidates
+
+
+
+
+
+
+
+
+
+
--- tests/testannotatorconfig.py
+++ tests/testannotatorconfig.py
@@ -5,6 +5,7 @@ import unittest
 from typing import Pattern
 
 from kirke.eblearn import annotatorconfig
+from kirke.sampleutils import regexgen
 
 def extract_str(pat: Pattern, line: str, group_num: int = 1) -> str:
     mat = re.search(pat, line)
@@ -13,6 +14,103 @@ def extract_str(pat: Pattern, line: str, group_num: int = 1) -> str:
         return mat_st
     return ''
 
+def extract_cand(alphanum: regexgen.RegexContextGenerator, line: str):
+    candidates,_ ,_ = alphanum.get_candidates_from_text(line, 1, [], [])
+    cand_text = ' /// '.join([cand['chars'] for cand in candidates])
+    return cand_text
+
+class TestAlphanum(unittest.TestCase):
+
+    def test_alphanum(self):
+        "Test AlphaNum"
+        alphanum = regexgen.RegexContextGenerator(0, 0, re.compile(r'(\+ \d[^\s]*|[^\s]*\d[^\s]*)'), 'idnum', join=True, length_min=2)
+
+        line = "text TIN: 555-77-5555 text"
+        self.assertEqual(extract_cand(alphanum, line), '555-77-5555')
+
+        line = "text EIN: 55-5555555 text"
+        self.assertEqual(extract_cand(alphanum, line), '55-5555555')
+
+        line = "text SSN: 555-55-5555 text"
+        self.assertEqual(extract_cand(alphanum, line), '555-55-5555')
+
+        line = "text ITIN: 955-77-5555 text"
+        self.assertEqual(extract_cand(alphanum, line), '955-77-5555')
+
+        line = "text ISIN: US0378331005 text"
+        self.assertEqual(extract_cand(alphanum, line), 'US0378331005')
+
+        line = "text CUSIP: 037833100 text"
+        self.assertEqual(extract_cand(alphanum, line), '037833100')
+
+        line = "text PHONE NUM: 1-201-505-6365 text"
+        self.assertEqual(extract_cand(alphanum, line), '1-201-505-6365')
+
+        line = "text PHONE NUM: 754-3010 text"
+        self.assertEqual(extract_cand(alphanum, line), '754-3010')
+
+        line = "text PHONE NUM: (541) 754-3010 text"
+        self.assertEqual(extract_cand(alphanum, line), '(541) 754-3010')
+
+        line = "text PHONE NUM: +1-541-754-3010 text"
+        self.assertEqual(extract_cand(alphanum, line), '+1-541-754-3010')
+
+        line = "text PHONE NUM: 001-541-754-3010 text"
+        self.assertEqual(extract_cand(alphanum, line), '001-541-754-3010')
+
+        line = "text PHONE NUM: 191 541 754 3010 text"
+        self.assertEqual(extract_cand(alphanum, line), '191 541 754 3010')
+
+        line = "text PHONE NUM: +49-89-636-48018 text"
+        self.assertEqual(extract_cand(alphanum, line), '+49-89-636-48018')
+
+        line = "text PHONE NUM: 19-49-89-636-48018 text"
+        self.assertEqual(extract_cand(alphanum, line), '19-49-89-636-48018')
+
+        line = "text PHONE NUM: (206)266-7010 text"
+        self.assertEqual(extract_cand(alphanum, line), '(206)266-7010')
+
+        line = "text PHONE NUM: 020 7524 6000 text"
+        self.assertEqual(extract_cand(alphanum, line), '020 7524 6000')
+
+        line = "text PHONE NUM: +44 20 7006 1000 text"
+        self.assertEqual(extract_cand(alphanum, line), '+44 20 7006 1000')
+
+        line = "text PHONE NUM: +44 (0)20 34001000 text"
+        self.assertEqual(extract_cand(alphanum, line), '+44 (0)20 34001000')
+
+        line = "text PHONE NUM: 1-800-KPMG text"
+        self.assertEqual(extract_cand(alphanum, line), '1-800-KPMG')
+
+        line = "text DOC ID: 23105963.3 text"
+        self.assertEqual(extract_cand(alphanum, line), '23105963.3')
+
+        line = "text DOC ID: #1470273vl text"
+        self.assertEqual(extract_cand(alphanum, line), '#1470273vl')
+
+        line = "text DOC ID: 70113 text"
+        self.assertEqual(extract_cand(alphanum, line), '70113')
+
+        line = "text DOC ID: L0001/06298/1726860 v.4 text"
+        self.assertEqual(extract_cand(alphanum, line), 'L0001/06298/1726860 v.4')
+
+        line = "text DOC ID: 3.4.1 text"
+        self.assertEqual(extract_cand(alphanum, line), '3.4.1')
+
+        line = "text ZIPCODE: 98109-5210 text"
+        self.assertEqual(extract_cand(alphanum, line), '98109-5210')
+
+        line = "text ZIPCODE: WC2N 5AF text"
+        self.assertEqual(extract_cand(alphanum, line), 'WC2N 5AF')
+
+        line = "text ZIPCODE: E34 53G E534 J2 "
+        self.assertEqual(extract_cand(alphanum, line), 'E34 53G E534 J2')
+
+        line = "text SECTION NUM: 1(a) text"
+        self.assertEqual(extract_cand(alphanum, line), '1(a)')
+
+        line = "text xx1, xx2, xx3 text"
+        self.assertEqual(extract_cand(alphanum, line), 'xx1 /// xx2 /// xx3')
 
 class TestCurrency(unittest.TestCase):
 
