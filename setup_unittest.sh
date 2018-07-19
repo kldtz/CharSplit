408fa67585f57a345f7a452e6440a6dec284e518
James Shaw <jshaw@ebrevia.com>
2018-07-19 16:28:03 -0400

    made the test documents available in s3

--- setup_unittest.sh
+++ setup_unittest.sh
@@ -1,9 +1,8 @@
 # to create the xxx.tar.gz
 # tar cvfz kirke_unit_test.v.0.1.tar.gz demo-pdf demo-txt export-train demo-validate dir-test-doc dir-scut-model eb_files_test
 
-wget kirke_unit_test.v.0.1.tar.gz
+wget https://s3.amazonaws.com/repo.ebrevia.com/repository/kirke_unit_test.v.0.1.tar.gz
 tar xvfz kirke_unit_test.v.0.1.tar.gz
-mkdir demo-out
 # nosetests tests
 # nosetests tests-ml
 
