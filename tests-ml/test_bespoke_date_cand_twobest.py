1fb1791d50eabef59702d62889ad8828400b47c8
emilytag <emily.tagtow@gmail.com>
2018-07-31 16:38:34 -0400

    added twobest

--- tests-ml/test_bespoke_date_cand_twobest.py
+++ tests-ml/test_bespoke_date_cand_twobest.py
@@ -27,7 +27,7 @@ class TestBespokeDate(unittest.TestCase):
             postfileutils.upload_train_dir(custid,
                                            custid_data_dir,
                                            candidate_types='DATE',
-                                           nbest=1)
+                                           nbest=2)
         ajson = json.loads(result_text)
         ant_result = ajson['en']
         
@@ -35,8 +35,8 @@ class TestBespokeDate(unittest.TestCase):
         print(ant_result)
 
         conf_matrix = ant_result['confusion_matrix']
-        # {'fn': 6, 'fp': 0, 'tn': 0, 'tp': 37}
-        # [[0, 0], [6, 37]]
+        # {'fn': 11, 'fp': 3, 'tn': 0, 'tp': 42}
+        # [[0, 3], [11, 42]]
 
         tn = conf_matrix[0][0]
         fp = conf_matrix[0][1]
@@ -44,25 +44,26 @@ class TestBespokeDate(unittest.TestCase):
         tp = conf_matrix[1][1]
 
         self.assertEqual(tn, 0)
-        self.assertAlmostEqual(fp, 0, delta=2)
-        self.assertAlmostEqual(fn, 6, delta=2)
-        self.assertAlmostEqual(tp, 37, delta=2)
+        self.assertAlmostEqual(fp, 3, delta=2)
+        self.assertAlmostEqual(fn, 11, delta=2)
+        self.assertAlmostEqual(tp, 42, delta=2)
 
         # round(ant_result['f1'], 2)
-        # 0.92
+        # 0.86
         f1 = round(ant_result['fscore'], 2)
-        self.assertGreaterEqual(f1, 0.90)
-        self.assertLessEqual(f1, 0.94)
+        self.assertGreaterEqual(f1, 0.84)
+        self.assertLessEqual(f1, 0.88)
 
         # round(ant_result['prec'], 2)
-        # 1.0
+        # 0.93
         precision = round(ant_result['precision'], 2)
-        self.assertGreaterEqual(precision, 0.98)
+        self.assertGreaterEqual(precision, 0.91)
+        self.assertLessEqual(precision, 0.95)
 
-        # 0.86
+        # 0.79
         recall = round(ant_result['recall'], 2)
-        self.assertGreaterEqual(recall, 0.84)
-        self.assertLessEqual(recall, 0.88)
+        self.assertGreaterEqual(recall, 0.77)
+        self.assertLessEqual(recall, 0.81)
 
 if __name__ == "__main__":
     unittest.main()
