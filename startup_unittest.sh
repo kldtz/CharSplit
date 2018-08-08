c2f51b9301c7f96c18c12c647e9af63cd0d04553
James Shaw <jshaw@ebrevia.com>
2018-08-08 17:20:33 +0000

    fixed a typo for EB_FILES

--- startup_unittest.sh
+++ startup_unittest.sh
@@ -3,8 +3,8 @@
 # https://github.com/scikit-learn/scikit-learn/issues/5115,
 # https://github.com/joblib/joblib/blob/master/doc/parallel.rst#bad-interaction-of-multiprocessing-and-third-party-libraries
 
-export EB_FILES=eb_files_test
-export EB_MODELS=dir-scut-model
+export EB_FILES=eb_files_test/
+export EB_MODELS=dir-scut-model/
 
 export JOBLIB_START_METHOD="forkserver"
 gunicorn --timeout 9600 app:app
