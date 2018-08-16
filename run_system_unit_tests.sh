f44e868b6a11c27144f10f6ff41774a66f387a1d
James Shaw <jshaw@ebrevia.com>
2018-08-16 01:11:30 -0400

    add more hints on unit testing

--- run_system_unit_tests.sh
+++ run_system_unit_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# run_pylint_all.sh
+# run_mypy_all.sh
+
 # assume corenlp server is running
 
 # wget https://s3.amazonaws.com/repo.ebrevia.com/repository/kirke_unit_test.v.0.1.tar.gz
