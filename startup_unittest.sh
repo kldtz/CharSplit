06b5a0f3ac09c6dfcef3c977a58155d12be20d47
James Shaw <jshaw@ebrevia.com>
2018-09-04 17:22:01 -0400

    updated test files for stratifiedgroupkfold

--- startup_unittest.sh
+++ startup_unittest.sh
@@ -3,7 +3,7 @@
 # https://github.com/scikit-learn/scikit-learn/issues/5115,
 # https://github.com/joblib/joblib/blob/master/doc/parallel.rst#bad-interaction-of-multiprocessing-and-third-party-libraries
 
-export EB_FILES=eb_files_test
+export EB_FILES=eb_files_test/
 export EB_MODELS=dir-scut-model
 
 export JOBLIB_START_METHOD="forkserver"
