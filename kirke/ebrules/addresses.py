fc99f11fd5bc9f6ec1dac88e833a75a9ff1531d8
emilytag <emily.tagtow@gmail.com>
2018-09-13 15:06:27 -0400

    final regex for addresses

--- kirke/ebrules/addresses.py
+++ kirke/ebrules/addresses.py
@@ -16,24 +16,25 @@ MIN_ADDRESS_LEN = 5
 MAX_ADDRESS_LEN = 100
 
 US_ZIP = '\d{5}(-\d{4})?'
-UK_STD = '[A-Z]{1,2}[0-9ROI][0-9A-Z]? *(?:(?![CIKMOV])[A-Z]?[0-9O][a-zA-Z]{2})'
-CAN_STD = '[A-Z][0-9][A-Z] +[0-9][A-Z][0-9]'
+UK_STD = '[A-Z]{1,2}[0-9ROI][0-9A-Z]?\s*(?:(?![CIKMOV])[A-Z]?[0-9O][a-zA-Z]{2})'
+CAN_STD = '[A-Z][0-9][A-Z]\s+[0-9][A-Z][0-9]'
 ZIP_CODE_YEAR = re.compile(r'\b\d{4,5}\b' + r'|\b{}\b'.format(UK_STD))
 
-LOCS = KEYWORDS['uk'] + KEYWORDS['us'] + KEYWORDS['can'] + KEYWORDS['country_names']
+LOCS = KEYWORDS['uk'] + KEYWORDS['us'] + KEYWORDS['can'] + KEYWORDS['country_names'] + ['London', 'LONDON']
 LOCS = sorted(LOCS, reverse = True)
+LOCS = "|".join(LOCS)
 
 # pylint: disable=too-many-locals
 def find_addresses(text: str) -> List[Tuple[int, int, str]]:
-    matches = re.finditer(r'(?=(\b(\d+|P\.? ?[0O]\.?|One) +.+?(\b{}\b|\b{}\b|\b{}\b)|\b(\d+|P\.? ?[0O]\.?|One) +.+?((\b({})\b)[,\. ]+)+))'.format(US_ZIP, UK_STD, CAN_STD, "|".join(LOCS)), text, re.DOTALL)
+    # finditer ( number anything (loc)+ zip )
+    matches = re.finditer(r'(?=(\b(\d+|P\.? ?[0O]\.?|One) +(.{{,80}}?\b({})\b)+?(.{{,80}}?(\b{}\b|\b{}\b|\b{}\b))?))'.format(LOCS, US_ZIP, UK_STD, CAN_STD), text, re.DOTALL)
     all_spans = [match.span(1) for match in matches]
     addr_se_st_list = []  # type: List[Tuple[int, int, str]]
     prev_start, prev_end, prev_prob = 0, 0, 0
     for ad_start, ad_end in all_spans:
         addr_st = text[ad_start:ad_end]
-        if len(addr_st.split()) > 3 and len(addr_st.split()) < 25:  # an address must have at least 4 words
+        if len(addr_st.split()) < 25:
             address_prob = classify(addr_st)
-            print(">>>>>>>", addr_st.replace("\n", " "), "<<", address_prob)
             if address_prob >= 0.5:
                 if ad_start > prev_start and ad_start < prev_end:
                     if address_prob >= prev_prob:
