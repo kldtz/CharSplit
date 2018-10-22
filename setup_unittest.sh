16009d6053c4b10c0df11333fce850a3519fb4eb
emilytag <emily.tagtow@gmail.com>
2018-10-22 22:17:00 +0000

    unit test setup wipes dirs

--- setup_unittest.sh
+++ setup_unittest.sh
@@ -1,6 +1,8 @@
 # to create the xxx.tar.gz
 # tar cvfz kirke_unit_test.v.0.1.tar.gz demo-pdf demo-txt export-train demo-validate dir-test-doc dir-scut-model eb_files_test cust_12345 cust_9 cands_json cust_10 cust_21 cust_22
 
+
+rm -rf eb_files_test/kirke_tmp/ dir-work dir-scut-model
 wget https://s3.amazonaws.com/repo.ebrevia.com/repository/kirke_unit_test.v.0.1.tar.gz
 tar xvfz kirke_unit_test.v.0.1.tar.gz
 # nosetests tests
