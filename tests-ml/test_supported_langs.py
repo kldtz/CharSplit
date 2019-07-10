9aa691200c7b5ae5806006006d218690daaa38df
James Shaw <jshaw@ebrevia.com>
2019-07-10 12:49:50 -0400

    double-byte char issue + minor adj due to observing line breaks

--- tests-ml/test_supported_langs.py
+++ tests-ml/test_supported_langs.py
@@ -32,13 +32,13 @@ class TestLangs(unittest.TestCase):
         corenlp_result = corenlputils.check_pipeline_lang('pt', pt_file)
 
         sents = corenlp_result['sentences']
-        ner_string = " ".join([tok['ner'] for tok in sents[3]['tokens']])
+        ner_string = ' '.join([tok['ner'] for tok in sents[3]['tokens']])
         # pylint: disable=line-too-long
         self.assertEqual(ner_string, 'O O O O O O O O LOCAL LOCAL LOCAL O O O O O ABSTRACCAO ABSTRACCAO ABSTRACCAO O ORGANIZACAO O O O O ORGANIZACAO O O O O O O O O O O O O O O')
-        ner_string = " ".join([tok['ner'] for tok in sents[42]['tokens']])
+        ner_string = ' '.join([tok['ner'] for tok in sents[42]['tokens']])
         # pylint: disable=line-too-long
         self.assertEqual(ner_string, 'O ORGANIZACAO O O PESSOA O O O PESSOA O O O O ORGANIZACAO ORGANIZACAO ORGANIZACAO ORGANIZACAO ORGANIZACAO ORGANIZACAO ORGANIZACAO ORGANIZACAO O')
-        ner_string = " ".join([tok['ner'] for tok in sents[241]['tokens']])
+        ner_string = ' '.join([tok['ner'] for tok in sents[241]['tokens']])
         # pylint: disable=line-too-long
         self.assertEqual(ner_string, 'O OBRA OBRA OBRA OBRA OBRA OBRA OBRA OBRA OBRA OBRA OBRA OBRA O O O O LOCAL LOCAL O O O O O O O VALOR O O O O O O')
 
@@ -56,14 +56,14 @@ class TestLangs(unittest.TestCase):
 
         corenlp_result = corenlputils.check_pipeline_lang('fr', fr_file)
         sents = corenlp_result['sentences']
-        ner_string = " ".join([tok['ner'] for tok in sents[26]['tokens']])
+        ner_string = ' '.join([tok['ner'] for tok in sents[26]['tokens']])
         # pylint: disable=line-too-long
         self.assertEqual(ner_string, 'O O O O O ORGANIZATION ORGANIZATION ORGANIZATION ORGANIZATION ORGANIZATION ORGANIZATION ORGANIZATION ORGANIZATION O O O O O O O O O O O O O DATE O')
-        ner_string = " ".join([tok['ner'] for tok in sents[217]['tokens']])
-        print(" ".join([tok['word'] + '/' + tok['ner'] for tok in sents[217]['tokens']]))
+        ner_string = ' '.join([tok['ner'] for tok in sents[217]['tokens']])
+        print(' '.join([tok['word'] + '/' + tok['ner'] for tok in sents[217]['tokens']]))
         self.assertEqual(ner_string, 'O O O O O O O O O O O O O O')
-        ner_string = " ".join([tok['ner'] for tok in sents[243]['tokens']])
-        print(" ".join([tok['word'] + '/' + tok['ner'] for tok in sents[243]['tokens']]))
+        ner_string = ' '.join([tok['ner'] for tok in sents[243]['tokens']])
+        print(' '.join([tok['word'] + '/' + tok['ner'] for tok in sents[243]['tokens']]))
         # pylint: disable=line-too-long
         self.assertEqual(ner_string, 'LOCATION LOCATION LOCATION LOCATION LOCATION LOCATION LOCATION LOCATION O ORGANIZATION ORGANIZATION O LOCATION')
 
@@ -80,14 +80,14 @@ class TestLangs(unittest.TestCase):
 
         corenlp_result = corenlputils.check_pipeline_lang('zh', zh_file)
         sents = corenlp_result['sentences']
-        ner_string = " ".join([tok['ner'] for tok in sents[20]['tokens']])
-        print(" ".join([tok['word'] + '/' + tok['ner'] for tok in sents[20]['tokens']]))
+        ner_string = ' '.join([tok['ner'] for tok in sents[21]['tokens']])
+        print(' '.join([tok['word'] + '/' + tok['ner'] for tok in sents[21]['tokens']]))
         self.assertEqual(ner_string, 'O O O O O O O O O O O O O O MISC O')
-        ner_string = " ".join([tok['ner'] for tok in sents[25]['tokens']])
-        print(" ".join([tok['word'] + '/' + tok['ner'] for tok in sents[25]['tokens']]))
+        ner_string = ' '.join([tok['ner'] for tok in sents[26]['tokens']])
+        print(' '.join([tok['word'] + '/' + tok['ner'] for tok in sents[26]['tokens']]))
         self.assertEqual(ner_string, 'O O O O MISC MISC MISC O O O O O O O O O O O O O MISC O')
-        ner_string = " ".join([tok['ner'] for tok in sents[28]['tokens']])
-        print(" ".join([tok['word'] + '/' + tok['ner'] for tok in sents[28]['tokens']]))
+        ner_string = ' '.join([tok['ner'] for tok in sents[29]['tokens']])
+        print(' '.join([tok['word'] + '/' + tok['ner'] for tok in sents[29]['tokens']]))
         self.assertEqual(ner_string, 'O O O O O O O O O O O O O O O O GPE O O O O O O O O')
 
         result_text = \
@@ -103,12 +103,12 @@ class TestLangs(unittest.TestCase):
 
         corenlp_result = corenlputils.check_pipeline_lang('es', es_file)
         sents = corenlp_result['sentences']
-        ner_string = " ".join([tok['ner'] for tok in sents[6]['tokens']])
+        ner_string = ' '.join([tok['ner'] for tok in sents[6]['tokens']])
         # pylint: disable=line-too-long
         self.assertEqual(ner_string, 'PERS PERS PERS O O O O ORG O O LUG LUG O O O O LUG LUG O O O O O O O O O O O LUG LUG O O O O LUG LUG O O OTROS O')
-        ner_string = " ".join([tok['ner'] for tok in sents[18]['tokens']])
+        ner_string = ' '.join([tok['ner'] for tok in sents[18]['tokens']])
         self.assertEqual(ner_string, 'O O O O O O O O O O O O ORG ORG PERS PERS PERS O O ORG ORG ORG O')
-        ner_string = " ".join([tok['ner'] for tok in sents[138]['tokens']])
+        ner_string = ' '.join([tok['ner'] for tok in sents[138]['tokens']])
         self.assertEqual(ner_string, 'PERS PERS PERS O O ORG ORG ORG ORG ORG O')
 
         result_text = \
@@ -127,12 +127,12 @@ class TestLangs(unittest.TestCase):
 
         corenlp_result = corenlputils.check_pipeline_lang('en', en_file)
         sents = corenlp_result['sentences']
-        ner_string = " ".join([tok['ner'] for tok in sents[257]['tokens']])
+        ner_string = ' '.join([tok['ner'] for tok in sents[257]['tokens']])
         # pylint: disable=line-too-long
         self.assertEqual(ner_string, 'O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O LOCATION LOCATION O O O ORGANIZATION ORGANIZATION ORGANIZATION O O O O O O O O O O O O O')
-        ner_string = " ".join([tok['ner'] for tok in sents[336]['tokens']])
+        ner_string = ' '.join([tok['ner'] for tok in sents[336]['tokens']])
         self.assertEqual(ner_string, 'ORGANIZATION ORGANIZATION ORGANIZATION O O')
-        ner_string = " ".join([tok['ner'] for tok in sents[362]['tokens']])
+        ner_string = ' '.join([tok['ner'] for tok in sents[362]['tokens']])
         self.assertEqual(ner_string, 'NUMBER O ORDINAL O O LOCATION LOCATION O LOCATION LOCATION')
 
         result_text = \
@@ -155,9 +155,11 @@ class TestLangs(unittest.TestCase):
         # maintly to test tokenized words
         # if the tokens are reasonable, then bespoke training can work
         # ok.  No NER.  All 'O' in kytea
-        gold_sent_0 = ['請負', '型', '／', '予定', '契約', '業務', '委託', '契約', '書']
+        # changed some characters to single-byte characters because
+        # internally, Kirke uses only single-byte characters.
+        gold_sent_0 = ['請負', '型', '/', '予定', '契約', '業務', '委託', '契約', '書']
 
-        gold_sent_12 = ['２', '前項', 'の', '規定', 'に', 'かかわ', 'ら', 'ず', '、', '仕様', '書',
+        gold_sent_12 = ['2', '前項', 'の', '規定', 'に', 'かかわ', 'ら', 'ず', '、', '仕様', '書',
                        'に', 'お', 'い', 'て', '本', '契約', 'の', '内容', 'と', '矛盾',
                        '・', '抵触', 'する', '内容', 'が', '定め', 'られ', 'た', '場合', '、',
                        '仕様', '書', 'に', 'お', 'け', 'る', '当該', '定め', 'は', 'その', '効力',
@@ -203,10 +205,12 @@ class TestLangs(unittest.TestCase):
         # maintly to test tokenized words
         # if the tokens are reasonable, then bespoke training can work
         # ok.  No NER.  All 'O' in kytea
-        gold_sent_6 = ['３', '乙', 'は', '、', '本件', '業務', 'の', '実施', 'に', '当た', 'り',
+        # changed some characters to single-byte characters because
+        # internally, Kirke uses only single-byte characters.
+        gold_sent_6 = ['3', '乙', 'は', '、', '本件', '業務', 'の', '実施', 'に', '当た', 'り',
                        '、', '以下', 'の', '(', '1', ')', '及び', '(', '2', ')', 'を', '遵守',
                        'する', 'もの', 'と', 'する', '。']
-        gold_sent_15 = ['２', '前項', 'の', '規定', 'に', 'かかわ', 'ら', 'ず', '、', '仕様', '書',
+        gold_sent_15 = ['2', '前項', 'の', '規定', 'に', 'かかわ', 'ら', 'ず', '、', '仕様', '書',
                        'に', 'お', 'い', 'て', '本', '契約', 'の', '内容', 'と', '矛盾',
                        '・', '抵触', 'する', '内容', 'が', '定め', 'られ', 'た', '場合', '、',
                        '仕様', '書', 'に', 'お', 'け', 'る', '当該', '定め', 'は', 'その', '効力',
