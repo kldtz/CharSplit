0da2ca33af79ff838d3db099a413518cddd48370
James Shaw <jshaw@ebrevia.com>
2019-07-23 20:13:37 -0400

    roll back setup_unittest.sh to pre-Japanese state

--- setup_unittest.sh
+++ setup_unittest.sh
@@ -3,21 +3,21 @@
 
 rm -rf eb_files_test/kirke_tmp/ dir-work dir-scut-model *tar.gz*
 
-# Note on why we switched back to 0.8.8 instead of 0.8.9
+# Note on why we switched back to 0.8.6 instead of 0.8.9
 #   - 0.8.9 version has some unit tests related to 'dates' updated (which is in
 #     Japanese branch) and they caused the unit tests to fail.
 #   - 0.8.9 has dir-korean/, which enable testing bestpoke training on korean documents.
 #
-# For now, simply check 0.8.8, copy the dir-korean/ from *.0.8.9.tar.gz and
+# For now, simply check 0.8.6, copy the dir-korean/ from *.0.8.9.tar.gz and
 # run the unit tests.  We want to verify korean training work in this PR.
 #
 # wget https://s3.amazonaws.com/repo.ebrevia.com/repository/kirke_unit_test.v.0.8.9.tar.gz
 #
 # tar xvfz kirke_unit_test.v.0.8.9.tar.gz
 
-wget https://s3.amazonaws.com/repo.ebrevia.com/repository/kirke_unit_test.v.0.8.8.tar.gz
+wget https://s3.amazonaws.com/repo.ebrevia.com/repository/kirke_unit_test.v.0.8.6.tar.gz
 
-tar xvfz kirke_unit_test.v.0.8.8.tar.gz
+tar xvfz kirke_unit_test.v.0.8.6.tar.gz
 
 # download the following file:
 # s3://repo.ebrevia.com/repository/dir-sent-check.v.0.1.tar.gz
