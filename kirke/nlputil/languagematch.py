888974d64e7a7c3152758f9e5aa4f4624a923da3
John Cowan <jcowan@ebrevia.com>
2019-08-22 16:12:24 -0400

    Filter match is the Right Thing

--- kirke/nlputil/languagematch.py
+++ kirke/nlputil/languagematch.py
@@ -1,5 +1,36 @@
 """RFC 4647 language tag matching."""
 
+def canonicalize(tag: str, default: str) -> str:
+    """Lowercase tag, and if it is None or empty, replace it with default."""
+    if not tag:
+        tag = default
+    return tag.lower().replace('_', '-')
+
+def language_basic_filter_match(needed: str, available: str, default: str = 'en') -> bool:
+    """Performs a filter match according to RFC 4647.
+       See <https://tools.ietf.org/html/rfc4647#section-3.4> for details.
+       The 'needed' argument is the language we need,
+       and corresponds to the 'language range' in the RFC.
+       The 'available' argument is what we have,
+       and corresponds to the 'language tag' in the RFC.
+       If either argument is None or empty, we assume the default.
+    """
+    # Canonicalize inputs
+    needed = canonicalize(needed, default)
+    available = canonicalize(available, default)
+
+    # Perform basic matching
+    if needed == '*':
+        # any available tag will do
+        return True
+    elif needed == available:
+        # exact match
+        return True
+    elif available.startswith(needed + '-'):
+        # accept a narrower (longer) available tag
+        return True
+    else:
+        return False
 
 def language_lookup_match(needed: str, available: str, default: str = 'en') -> bool:
     """Performs a lookup match according to RFC 4647.
@@ -11,29 +42,24 @@ def language_lookup_match(needed: str, available: str, default: str = 'en') -> b
        If either argument is None or empty, we assume the default.
     """
 
-    # Canonicalize arguments
-    if not needed:
-        needed = default
-    needed = needed.lower().replace('_', '-')
-    needed_subtags = needed.split('-')
-    if not available:
-        available = default
-    available = available.lower().replace('_', '-')
-    available_subtags = available.split('-')
+    # Split canonicalized arguments
+    needed_subtags = canonicalize(needed, default).split('-')
+    available_subtags = canonicalize(available, default).split('-')
 
     # Match plain "*" tag, but not any other "*" subtags
-    if "*" in needed:
-      return len(needed) == 1
+    if "*" in needed_subtags:
+        return len(needed_subtags) == 1
 
-    # Try to match. If we fail, reduce 'needed' by one step
+    # Try to match. If we fail, reduce 'available' by one step
+    # (going from more specific to less specific) and try again.
     while needed_subtags:
         if needed_subtags == available_subtags:
-             return True
+            return True
         needed_subtags = needed_subtags[:-1]
         # Remove a single-letter tag if present
         try:
             if len(needed_subtags[-1]) == 1:
-              needed_subtags = needed_subtags[:-1]
+                needed_subtags = available_subtags[:-1]
         except IndexError:
-              pass
+            pass
     return False
--- tests/test_languagematch.py
+++ tests/test_languagematch.py
@@ -1,27 +1,65 @@
+# pylint: disable=missing-docstring,wildcard-import
 import unittest
 from kirke.nlputil.languagematch import *
 
-class TestLanguageLookupMatch:
+class TestCanonicalize(unittest.TestCase):
 
-    def simple_match(self):
-        self.assertTrue(needed='', available='')
-        self.assertTrue(needed='en', available='')
-        self.assertTrue(needed=None, available='en')
-        self.assertTrue(needed='en-us', available='en_us')
-        self.assertFalse(needed='en', available='de')
-        self.assertFalse(needed='', available='de')
+    def test_canonicalize(self):
+        self.assertEqual(canonicalize(None, 'en'), 'en')
+        self.assertEqual(canonicalize('', 'en'), 'en')
+        self.assertEqual(canonicalize('en-US', 'en'), 'en-us')
+        self.assertEqual(canonicalize('en_US', 'en'), 'en-us')
 
-    def reduction_match(self):
-        self.assertTrue(needed='en', available='en-us')
-        self.assertFalse(needed='en-us', available='en')
+class TestLanguageBasicFilterMatch(unittest.TestCase):
 
-    def star_match(self):
-        self.assertTrue(needed='*', available='ja')
-        self.assertFalse(needed='en-*', available='en')
+    def test_simple_match(self):
+        self.assertTrue(language_basic_filter_match('', ''))
+        self.assertTrue(language_basic_filter_match('en', ''))
+        self.assertTrue(language_basic_filter_match(None, 'en'))
+        self.assertTrue(language_basic_filter_match('en-us', 'en_us'))
+        self.assertFalse(language_basic_filter_match('en', 'de'))
+        self.assertFalse(language_basic_filter_match('', 'de'))
 
-    def singleton_match(self):
-      self.assertTrue(needed='en', available='en-x-nyc')
-      self.assertFalse(needed='en-x', available='en-x-nyc')
+    def test_equality_match(self):
+        self.assertTrue('de', 'de')
 
-    def gonna_fail(self):
-        self.assertTrue(False)
+    def test_extension_match(self):
+        # We need generic English, we only have American English: reject.
+        self.assertFalse(language_lookup_match('en', 'en-us'))
+        # We need American English, we have generic English: accept.
+        self.assertTrue(language_lookup_match('en-us', 'en'))
+        # Total mismatch
+        self.assertFalse(language_lookup_match('en-us', 'en-uk'))
+
+    def test_star_match(self):
+        self.assertTrue(language_lookup_match('*', 'ja'))
+        self.assertFalse(language_lookup_match('en-*', 'en'))
+
+
+class TestLanguageLookupMatch(unittest.TestCase):
+
+    def test_simple_match(self):
+        self.assertTrue(language_lookup_match('', ''))
+        self.assertTrue(language_lookup_match('en', ''))
+        self.assertTrue(language_lookup_match(None, 'en'))
+        self.assertTrue(language_lookup_match('en-us', 'en_us'))
+        self.assertFalse(language_lookup_match('en', 'de'))
+        self.assertFalse(language_lookup_match('', 'de'))
+
+    def test_reduction_match(self):
+        # We need something en-us, en is the best we can do: accept.
+        self.assertTrue(language_lookup_match('en-us', 'en'))
+        # We need something en, we have en-us: too specific, reject.
+        self.assertFalse(language_lookup_match('en', 'en-us'))
+        # US English and UK English just don't match
+        self.assertFalse(language_lookup_match('en-us', 'en-uk'))
+
+    def test_star_match(self):
+        self.assertTrue(language_lookup_match('*', 'ja'))
+        self.assertFalse(language_lookup_match('en-*', 'en'))
+
+    def test_singleton_match(self):
+        # We need NYC English, we only have generic English: reject.
+        self.assertFalse(language_lookup_match('en-x-nyc', 'en'))
+        # Misuse of singleton
+        self.assertTrue(language_lookup_match('en-x-nyc', 'en-x'))
